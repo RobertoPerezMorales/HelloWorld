9825c0a7f009bfbfabbc2ab620e85b433ea1ac13
ic070_14 <ic070_14@china.ece.uprm.edu>
2016-09-19 15:16:47 -0400

    Added some display text.

--- MyProjectIsBetter/src/Classic.java
+++ MyProjectIsBetter/src/Classic.java
@@ -5,13 +5,13 @@ public class Classic {
 		// TODO Auto-generated method stub
 
 		long timeBefore = System.nanoTime();
-		for (int i = 0; i<10000000; i++){
-			System.out.println(i);
+		for (int i = 0; i<1000000; i++){
+			System.out.println(i+1);
 			
 		}
 		long timeAfter = System.nanoTime();
 		
-		System.out.println("Time in seconds is " + (timeAfter-timeBefore)/1000000000);
+		System.out.println("Time in seconds is " + (timeAfter-timeBefore)/1000000000.00 + "s");
 		
 	}
 
